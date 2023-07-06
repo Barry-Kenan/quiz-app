d7b0e3283ed39e3b580f48a6ad3a0c7b62485af9
Kenan <bary.berdiyev.97@mail.ru>
2023-07-06 11:32:51 +0700

    получение вопросов перемещен на StartPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,12 @@
 import 'antd/dist/reset.css';
-import { useActions } from 'hooks/action';
 import { useAppSelector } from 'hooks/redux';
 import { withLayout } from 'layout/Layout';
 import { FinishPage, GamePage, StartPage } from 'pages';
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 
 const App: FC = () => {
 	const { status } = useAppSelector(state => state.quizReducer);
-	const { getQuestionsAction } = useActions();
 
-	useEffect(() => {
-		getQuestionsAction();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
 	switch (status) {
 		case 'start':
 			return <StartPage />;
--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,15 +1,20 @@
 import { Button, Typography } from 'antd';
 import { useActions } from 'hooks/action';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import styles from './StartPage.module.scss';
 
 const { Title } = Typography;
 
 const StartPage: FC = () => {
-	const { changeStatus } = useActions();
+	const { changeStatus, getQuestionsAction } = useActions();
 	const handleClick = () => {
 		changeStatus('play');
 	};
+
+	useEffect(() => {
+		getQuestionsAction({ page: 1, pageSize: 5 });
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 	return (
 		<div className={styles.wrapper}>
 			<Title level={2}>Quiz</Title>
