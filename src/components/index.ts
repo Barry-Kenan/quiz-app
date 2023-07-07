5a7c85c4c63026a206bacd40d2c24258835cd716
Kenan <bary.berdiyev.97@mail.ru>
2023-07-07 10:15:50 +0700

    экспорт компонент

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as FinishQuiz } from './FinishQuiz/FinishQuiz';
 export { default as Loading } from './Loading/Loading';
 export { default as QuestionCard } from './QuestionCard/QuestionCard';
+export { default as QuestionsForm } from './QuestionsForm/QuestionsForm';
+export { default as QuestionsPagination } from './QuestionsPagination/QuestionsPagination';
