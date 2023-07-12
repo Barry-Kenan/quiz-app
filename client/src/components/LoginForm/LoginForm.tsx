f87f7635240a658cb854ec77ca52c71a764150b9
Kenan <bary.berdiyev.97@mail.ru>
2023-07-12 15:48:11 +0700

    форма для логина

---
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,92 @@
+import { Button, Input } from 'antd';
+import { useActions } from 'hooks/action';
+import { IUser } from 'interfaces/user.interface';
+import { FC } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import styles from './LoginForm.module.scss';
+
+const LoginForm: FC = () => {
+	const { login } = useActions();
+	const {
+		control,
+		reset,
+		clearErrors,
+		formState: { errors },
+		handleSubmit
+	} = useForm<Omit<IUser, 'id' | 'name'>>();
+
+	const onSubmit = (formData: Omit<IUser, 'id' | 'name'>) => {
+		login(formData);
+		reset();
+	};
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+			<Controller
+				name='email'
+				control={control}
+				rules={{
+					required: {
+						value: true,
+						message: 'Введите email'
+					},
+					validate: {
+						maxLength: v =>
+							v.length <= 50 || 'The email should have at most 50 characters',
+						matchPattern: v =>
+							/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+							'Email address must be a valid address'
+					}
+				}}
+				render={({ field }) => (
+					<>
+						<Input
+							onChange={field.onChange}
+							value={field.value}
+							status={errors.email && 'error'}
+							size='large'
+							placeholder='sputnik@mail.ru'
+						/>
+						{errors.email && (
+							<span className={styles.error}> {errors.email.message}</span>
+						)}
+					</>
+				)}
+			/>
+			<Controller
+				name='password'
+				control={control}
+				rules={{
+					required: {
+						value: true,
+						message: 'Введите пароль'
+					}
+				}}
+				render={({ field }) => (
+					<>
+						<Input.Password
+							onChange={field.onChange}
+							value={field.value}
+							status={errors.password && 'error'}
+							size='large'
+							placeholder='*******'
+						/>
+						{errors.password && (
+							<span className={styles.error}> {errors.password.message}</span>
+						)}
+					</>
+				)}
+			/>
+			<Button
+				type='primary'
+				htmlType='submit'
+				onClick={() => clearErrors()}
+				size='large'
+			>
+				Отправить
+			</Button>
+		</form>
+	);
+};
+
+export default LoginForm;
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as FinishQuiz } from './FinishQuiz/FinishQuiz';
 export { default as Loading } from './Loading/Loading';
+export { default as LoginForm } from './LoginForm/LoginForm';
 export { default as QuestionCard } from './QuestionCard/QuestionCard';
 export { default as QuestionsForm } from './QuestionsForm/QuestionsForm';
 export { default as QuestionsPagination } from './QuestionsPagination/QuestionsPagination';
