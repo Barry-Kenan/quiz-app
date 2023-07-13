6579f7ae5c2ecd39d6dfb85ae338e5ec8598bce3
Kenan <bary.berdiyev.97@mail.ru>
2023-07-13 10:16:09 +0700

    путь исправлен

--- client/src/store/actions/auth/auth.actions.ts
+++ client/src/store/actions/auth/auth.actions.ts
@@ -1,5 +1,5 @@
 import { IUser } from 'interfaces/user.interface';
-import { AuthAction, AuthActionEnum } from '../../action-types/auth/auth.types';
+import { AuthAction, AuthActionEnum } from 'store/action-types/auth/auth.types';
 
 const setUser = (payload: Omit<IUser, 'password'>): AuthAction => ({
 	type: AuthActionEnum.SET_USER,
