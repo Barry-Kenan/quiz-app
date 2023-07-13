2e84d560c1f3627e1de93cdcaa73101c9bf8d524
Kenan <bary.berdiyev.97@mail.ru>
2023-07-13 08:17:30 +0700

    logout

--- client/src/layout/Header/Header.tsx
+++ client/src/layout/Header/Header.tsx
@@ -1,12 +1,28 @@
+import { Button, Typography } from 'antd';
 import cn from 'classnames';
+import { useActions } from 'hooks/action';
+import { useAppSelector } from 'hooks/redux';
 import styles from './Header.module.scss';
 import { HeaderProps } from './Header.props';
 import Logo from './logo.svg';
 
+const { Title } = Typography;
 const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
+	const { isAuth, name } = useAppSelector(state => state.authReducer);
+	const { logout } = useActions();
 	return (
 		<header className={cn(className, styles.header)} {...props}>
 			<Logo />
+			{isAuth && (
+				<div className={styles.block}>
+					<Title level={4} mark className={styles.title}>
+						{name}
+					</Title>
+					<Button type='primary' onClick={logout}>
+						Выход
+					</Button>
+				</div>
+			)}
 		</header>
 	);
 };
