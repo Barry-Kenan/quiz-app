2fd597ced7ab4539ff6163cbeb03d7b5d3358da9
Kenan <bary.berdiyev.97@mail.ru>
2023-07-13 08:16:31 +0700

    защита страниц

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,4 +1,6 @@
 import 'antd/dist/reset.css';
+import AuthRoutes from 'helpers/guards/AuthRoutes';
+import PrivateRoutes from 'helpers/guards/PrivateRoutes';
 import { withLayout } from 'layout/Layout';
 import { HomePage, LoginPage, RegisterPage } from 'pages';
 import { FC } from 'react';
@@ -8,9 +10,13 @@ const App: FC = () => {
 	return (
 		<>
 			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/register' element={<RegisterPage />} />
+				<Route element={<PrivateRoutes />}>
+					<Route path='/' element={<HomePage />} />
+				</Route>
+				<Route element={<AuthRoutes />}>
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/register' element={<RegisterPage />} />
+				</Route>
 				<Route path='*' element={<p>There's nothing here: 404!</p>} />
 			</Routes>
 		</>
