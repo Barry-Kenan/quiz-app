886d05c2082a334221c93b396779e3c506b175c5
Kenan <bary.berdiyev.97@mail.ru>
2023-07-14 12:33:42 +0700

    timer

--- client/src/components/Timer/Timer.props.ts
+++ client/src/components/Timer/Timer.props.ts
@@ -3,4 +3,5 @@ import { DetailedHTMLProps, HTMLAttributes } from 'react';
 export interface TimerProps
 	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
 	time: number;
+	countDown: number;
 }
--- client/src/components/Timer/Timer.tsx
+++ client/src/components/Timer/Timer.tsx
@@ -1,19 +1,23 @@
 import { Progress } from 'antd';
+import { secondsToHms } from 'helpers/quiz';
 import { FC } from 'react';
 import styles from './Timer.module.scss';
 import { TimerProps } from './Timer.props';
 
-const Timer: FC<TimerProps> = ({ time }) => {
-	if (time == 0) {
+const Timer: FC<TimerProps> = ({ time, countDown }) => {
+	if (countDown == 0) {
 		return <></>;
 	}
 
+	const forPercent = 100 / time;
 	return (
 		<div className={styles.timer}>
 			<Progress
 				type='circle'
-				percent={time * 5}
-				format={() => <span className={styles.time}>{time}</span>}
+				percent={countDown * forPercent}
+				format={() => (
+					<span className={styles.time}>{secondsToHms(countDown)}</span>
+				)}
 				strokeColor={{ '0%': '#1677ff', '100%': '#1677ff' }}
 			/>
 		</div>
