3d850899b2e519f7be3252fa49fbafa8823e4f79
Kenan <bary.berdiyev.97@mail.ru>
2023-07-14 12:30:03 +0700

    страница auth

---
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -0,0 +1,43 @@
+import { LoginOutlined, UserAddOutlined } from '@ant-design/icons';
+import { Button, Modal } from 'antd';
+import { useActions } from 'hooks/action';
+import LoginPage from 'pages/LoginPage/LoginPage';
+import RegisterPage from 'pages/RegisterPage/RegisterPage';
+import { FC, useEffect, useState } from 'react';
+import styles from './AuthPage.module.scss';
+
+const AuthPage: FC = () => {
+	const { auth } = useActions();
+	const [modal, setModal] = useState<null | 'login' | 'register'>(null);
+
+	useEffect(() => {
+		auth();
+	}, []);
+
+	const handleCancel = () => {
+		setModal(null);
+	};
+
+	return (
+		<div className={styles.wrapper}>
+			<Button type='link' size='large' onClick={() => setModal('login')}>
+				Login <LoginOutlined />
+			</Button>
+			<Button type='link' size='large' onClick={() => setModal('register')}>
+				Register <UserAddOutlined />
+			</Button>
+			<Modal
+				open={!!modal}
+				onCancel={handleCancel}
+				centered
+				footer={null}
+				closeIcon={true}
+			>
+				{modal == 'login' && <LoginPage />}
+				{modal == 'register' && <RegisterPage />}
+			</Modal>
+		</div>
+	);
+};
+
+export default AuthPage;
