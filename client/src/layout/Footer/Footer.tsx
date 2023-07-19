eaab708a6a59addf49e3b61ac1236c03195465bf
Kenan <bary.berdiyev.97@mail.ru>
2023-07-19 19:26:35 +0700

    добавлена ссылка на github

--- client/src/layout/Footer/Footer.tsx
+++ client/src/layout/Footer/Footer.tsx
@@ -1,11 +1,16 @@
+import { GithubOutlined } from '@ant-design/icons';
 import cn from 'classnames';
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
 import { FooterProps } from './Footer.props';
 
 const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
 		<footer className={cn(className, styles.footer)} {...props}>
-			<span>Barry Kenan</span>
+			<Link to='https://github.com/Barry-Kenan' target='_blank'>
+				<GithubOutlined />
+				Barry Kenan
+			</Link>
 		</footer>
 	);
 };
