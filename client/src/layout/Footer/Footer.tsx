8efd719b999236e9a94e1ab7f5527fd26b171701
Kenan <bary.berdiyev.97@mail.ru>
2023-07-26 11:09:22 +0700

    добавлен пробел

--- client/src/layout/Footer/Footer.tsx
+++ client/src/layout/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { GithubOutlined } from '@ant-design/icons';
+import { Space } from 'antd';
 import cn from 'classnames';
 import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
@@ -8,8 +9,10 @@ const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
 		<footer className={cn(className, styles.footer)} {...props}>
 			<Link to='https://github.com/Barry-Kenan' target='_blank'>
-				<GithubOutlined />
-				Barry Kenan
+				<Space>
+					<GithubOutlined />
+					Barry_Kenan
+				</Space>
 			</Link>
 		</footer>
 	);
