c518f7e5e9f3a1226dd498d827c3947746d17d2c
Kenan <bary.berdiyev.97@mail.ru>
2023-07-26 17:44:32 +0700

    хук скролла по y

---
+++ client/src/hooks/useScrollY.ts
@@ -0,0 +1,17 @@
+import { MutableRefObject, useEffect, useState } from 'react';
+
+export const useScrollY = (ref: MutableRefObject<HTMLDivElement>): number => {
+	const [scrollY, setScrollY] = useState<number>(0);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const currentScrollY = ref.current.scrollTop;
+			setScrollY(currentScrollY);
+		};
+		ref.current.addEventListener('scroll', handleScroll, { passive: true });
+		return () => {
+			ref.current?.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+	return scrollY;
+};
