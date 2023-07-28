940383c7056c8d94bd9e6ed9dc7121d23461a995
Kenan <bary.berdiyev.97@mail.ru>
2023-07-28 16:56:11 +0700

    убран скроллинг при переходе на другую страницу

--- client/src/components/QuestionsPagination/QuestionsPagination.tsx
+++ client/src/components/QuestionsPagination/QuestionsPagination.tsx
@@ -1,5 +1,4 @@
 import { Pagination } from 'antd';
-import { scrollToTop } from 'helpers/quiz';
 import { useActions } from 'hooks/action';
 import { useAppSelector } from 'hooks/redux';
 import { GameContext } from 'modules/Game/context/Game.context';
@@ -14,7 +13,7 @@ import styles from './QuestionsPagination.module.scss';
 const QuestionsPagination: FC = () => {
 	const { questionsCount } = useAppSelector(state => state.quizReducer);
 	const { getQuestionsAction } = useActions();
-	const { questionsDivRef, setIsLastPage } = useContext(GameContext);
+	const { setIsLastPage } = useContext(GameContext);
 
 	const isLastPage = (pageSize: number, page: number) => {
 		const isLast = Math.ceil(questionsCount / pageSize) == page;
@@ -25,7 +24,6 @@ const QuestionsPagination: FC = () => {
 	const onChange = (page: number, pageSize: number) => {
 		getQuestionsAction({ page, pageSize });
 		setIsLastPage(isLastPage(pageSize, page));
-		scrollToTop(questionsDivRef);
 	};
 
 	return (
